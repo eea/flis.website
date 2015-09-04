113c2fdac45e71d546f93761f800f0da402c0e95
Valentin Popescu <ppsc.valentin@gmail.com>
2015-09-04 11:42:23 +0300

    Updated header with account stuff

--- js/main.js
+++ js/main.js
@@ -8,34 +8,50 @@ $(document).ready(function() {
   $(".dataTable").DataTable( dataTableSettings );
 
   // Navmenu toggle
+  var openedMenu;
 
-  var $target = $("#navmenu");
+  function bodyClass(id) {
+    return id.replace("#", "") + "_is_open";
+  }
 
-  function openNavmenu() {
-    $target
+  function openMenu(target) {
+    $(target)
       .addClass("is_open")
       .on("click", function(e) {
         e.stopPropagation();
       });
-    $("body").addClass("navmenu_is_open");
+
+    $("[data-toggle='" + target + "']").addClass("is_toggled");
+    $("body").addClass(bodyClass(target));
+    openedMenu = target;
   }
 
-  function closeNavmenu() {
-    $target.removeClass("is_open");
-    $("body").removeClass("navmenu_is_open");
+  function closeMenu(target) {
+    if (!target && openedMenu) {
+      target = openedMenu;
+    }
+    if (target) {
+      $(target).removeClass("is_open");
+      $("[data-toggle='" + target + "']").removeClass("is_toggled");
+      $("body").removeClass(bodyClass(target));
+    }
   }
   
-  $("#navmenu-toggle").on("click", function(e) {
+  $("[data-toggle]").on("click", function(e) {
     e.stopPropagation();
+    target = $(this).data("toggle");
 
-    if ($target.hasClass("is_open")) {
-      closeNavmenu();
+    if ($(target).hasClass("is_open")) {
+      closeMenu(target);
     } else {
-      openNavmenu();
+      closeMenu();
+      openMenu(target);
     }
     
   });
 
-  $("body").on("click", closeNavmenu);
+  $("body").on("click", function() { 
+    closeMenu();
+  });
 
 });
